872bbee638d230e557d9185d155cbe60b7120918
CarlRobertMots <robertm6ts@gmail.com>
2025-10-30 09:44:20 +0200

    Add,edit,delete Person

---
+++ main/java/module-info.java
@@ -0,0 +1,9 @@
+module ch.makery.address {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ch.makery.address.view to javafx.fxml;
+    exports ch.makery.address;
+    exports ch.makery.address.view;
+}
